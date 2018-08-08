c68305700e15f5415842e9a440fd58b6b83f5040
Aman Vijay <amanvijay.cse19@jecrc.ac.in>
2018-08-09 00:45:49 +0530

    take user input and plot a graph for it

---
+++ user_input_graph.py
@@ -0,0 +1,49 @@
+import pandas as pd
+pd.core.common.is_list_like = pd.api.types.is_list_like
+
+import pandas_datareader.data as web
+import datetime
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+app = dash.Dash()
+server = app.server
+
+app.layout = html.Div(children=[
+    html.Div(children='''
+        Symbol to graph:
+    '''),
+    dcc.Input(id='input', value='', type='text'),
+    html.Div(id='output-graph'),
+])
+
+@app.callback(
+    Output(component_id='output-graph', component_property='children'),
+    [Input(component_id='input', component_property='value')]
+)
+def update_value(input_data):
+    #input_data = str(input_data).upper() 
+    start = datetime.datetime(2015, 1, 1)
+    end = datetime.datetime.now()
+    start = datetime.datetime(2015, 1, 1)
+    end = datetime.datetime.now()
+    df = web.DataReader("TSLA", 'quandl', start, end)
+    df.reset_index(inplace=True)
+    df.set_index("Date", inplace=True)
+
+    return dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': df.index, 'y': df.Close, 'type': 'line', 'name': input_data},
+            ],
+            'layout': {
+                'title': input_data
+            }
+        }
+    )
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
